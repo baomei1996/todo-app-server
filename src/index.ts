15d08b8dae8208e5a72b0a9f559b2d7fedd0ed2e
baomei1996 <qhal4686@gmail.com>
2023-11-28 23:12:36 +0900

    Feat: custom ts config

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+
+// create a server
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/", (req, res) => {
+    // here we need data so that we can create new note/todo
+    console.log(req.body);
+
+    res.json({
+        message: "I am a post request",
+    });
+});
+
+// listen to some port
+app.listen(8000, () => {
+    console.log("Server is listening on port 8000");
+});
